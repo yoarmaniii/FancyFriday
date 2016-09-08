7b97e2d1ba341d0b684d1d2878cf6bbf39171122
Armani Raleigh <armanijolee@gmail.com>
2016-09-08 11:23:08 -0400

    Initial commit Fancy Friday

---
+++ src/mainApp.java
@@ -0,0 +1,20 @@
+/**
+
+ * This a demo application for deciding what to wear.
+ */
+
+/**
+ * @author TheCreativeSuiteMAC
+ *
+ */
+public class mainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
